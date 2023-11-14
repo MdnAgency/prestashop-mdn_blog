5bc1df5e8fb5f2f0db58e74df3c6bcd93b943a72
Loris Pinna <shiny74@hotmail.fr>
2023-11-14 13:48:11 +0100

    :bug: Fix a crash

--- controllers/admin/AdminBlogController.php
+++ controllers/admin/AdminBlogController.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once _PS_MODULE_DIR_ . '/mdn_blog/classes/BlogCategoryModel.php';
-require_once "AdminBlogCategoryController.php";
+require_once "AdminBlogArticleController.php";
 
 class AdminBlogController extends AdminBlogArticleController
 {
